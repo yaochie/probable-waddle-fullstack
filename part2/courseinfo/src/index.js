c1565f2c61b938bea670e2d4aa1afce2a5134006
Yao Chong <10258156+yaochie@users.noreply.github.com>
2020-04-16 19:44:01 -0400

    exercise 2.4

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = ({ text }) => {
-  return (
-    <h1>{text}</h1>
-  )
-}
+const Title = ({ text }) => <h1>{text}</h1>
+
+const Header = ({ text }) => <h2>{text}</h2>
 
 const Total = ({ sum }) => {
   return(
-    <h4>total of {sum} exercises</h4>
+    <b>total of {sum} exercises</b>
   ) 
 }
 
@@ -45,36 +43,58 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Debugging',
-        exercises: 5,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
 
   return (
     <div>
-      <Course course={course} />
+      <Title text="Web dev curriculum" />
+      {courses.map(
+        course => <Course key={course.id} course={course} />
+      )}
     </div>
   )
 }
