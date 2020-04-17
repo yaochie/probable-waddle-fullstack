6cbe1216c8258b89137c9cb174984670196f7dc4
Yao Chong <10258156+yaochie@users.noreply.github.com>
2020-04-16 20:29:03 -0400

    exercise 2.7

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -8,6 +8,13 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
+
+    // prevent duplicate people
+    if (persons.map(person => person.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
     const personObject = {
       name: newName,
     }
