c89854046a1718f630c83519433d885c2cb613df
Yao Chong <10258156+yaochie@users.noreply.github.com>
2020-04-17 16:20:46 -0400

    exercise 2.14

--- part2/countries/src/components/SingleCountry.js
+++ part2/countries/src/components/SingleCountry.js
@@ -1,6 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const WeatherMap = ({ weather }) => {
+  if (weather === null) {
+    return <></>
+  }
+
+  const icons = weather.weather.map((w, index) => {
+    return (
+      <img
+        key={index}
+        src={`https://openweathermap.org/img/wn/${w.icon}@2x.png`}
+        alt={w.description}
+      />
+    )
+  })
+
+  return (
+    <>
+      <div><b>temperature:</b> {weather.main.temp} celsius</div>
+      <div>
+        {icons}
+      </div>
+      <div>
+        <b>Wind:</b> {weather.wind.speed} m/s at {weather.wind.deg} degrees
+      </div>
+    </>
+  )
+}
 
 const SingleCountry = ({ country }) => {
+  const [weather, setWeather] = useState(null)
+  const api_key = process.env.REACT_APP_API_KEY
+
+  const weatherQuery = "https://api.openweathermap.org/data/2.5/weather"
+        + "?q=" + country.capital
+        + "&units=metric"
+        + "&appid=" + api_key
+
+  useEffect(() => {
+    axios
+      .get(weatherQuery)
+      .then(response => setWeather(response.data))
+  }, [])
+
   return (
     <div>
       <h1>{country.name}</h1>
@@ -20,6 +63,8 @@ const SingleCountry = ({ country }) => {
           alt={`${country.name}'s flag`}
         />
       </div>
+      <h2>Weather in {country.capital}</h2>
+      <WeatherMap weather={weather} />
     </div>
   )
 }
