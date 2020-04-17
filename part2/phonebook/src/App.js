e81a76b04f4588b582b51e4f419ab8722988f330
Yao Chong <10258156+yaochie@users.noreply.github.com>
2020-04-16 22:00:18 -0400

    exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,14 @@ import Numbers from './components/Numbers'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "040-1234567" }
+    { name: 'Arto Hellas', number: "040-1234567" },
+    { name: 'Ada Lovelace', number: '39-44-5323523'},
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ search, setSearch ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -36,9 +40,16 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    setSearch(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input value={search} onChange={handleSearchChange} />
+      <h2>Add a new number</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -50,7 +61,7 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <Numbers persons={persons} />
+      <Numbers persons={persons} search={search} />
     </div>
   )
 }
--- part2/phonebook/src/components/Numbers.js
+++ part2/phonebook/src/components/Numbers.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const Numbers = ({ persons }) => {
+const Numbers = ({ persons, search }) => {
+  const personsToShow = (search === "")
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <>
       <h2>Numbers</h2>
-      {persons.map(person => 
+      {personsToShow.map(person => 
         <div key={person.name}>{person.name} {person.number}</div>
       )}
     </>
